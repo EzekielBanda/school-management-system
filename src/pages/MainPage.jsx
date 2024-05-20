a6e1b3d6ceab4e4b6f7b7e070bb775dcf1341f6b
EzekielBanda <bsc-com-10-19@unima.ac.mw>
2024-05-20 22:44:43 +0200

    partially implemented add student form

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 import Dashboard from '../components/Dashboard';
 import TopBar from "../components/navbar/TopBar";
-import StudentForm from "../components/students/StudentForm"
-import Parent from "../components/parents/Parent"
+import StudentForm from "../components/students/StudentForm";
+import Parent from "../components/parents/Parent";
 import { Routes, Route } from 'react-router-dom';
 
 const MainPage = () => {
@@ -15,15 +15,15 @@ const MainPage = () => {
         <Sidebar />
         <div className="flex-grow ml-64 md:ml-0">
           <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="students" element={<StudentForm />} />
-              <Route path="parents" element={<Parent/>}/>
-              {/* Add more nested routes here */}
-            </Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="students" element={<StudentForm />} />
+            <Route path="parents" element={<Parent />} />
+            {/* Add more nested routes here */}
+          </Routes>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default MainPage;
